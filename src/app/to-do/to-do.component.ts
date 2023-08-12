b727f7f3977d80383275272e1392650fff5d71a8
Arjun Mishra <142076777+Arjun-Mishra07@users.noreply.github.com>
2023-08-12 12:00:10 +0530

    Add files via upload

---
+++ src/app/to-do/to-do.component.ts
@@ -0,0 +1,61 @@
+import { itask } from './../model/task'
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
+@Component({
+  selector: 'app-to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css']
+})
+export class ToDoComponent {
+  todoForm!: FormGroup;
+  tasks: itask[] = [];
+  done: itask[] = [];
+  updateIndex!: any;
+  editEnabled: boolean = false;
+  constructor(private frmb: FormBuilder) { }
+  ngOnInit(): void {
+    this.todoForm = this.frmb.group({
+      item: ['', Validators.required]
+    })
+
+  }
+  drop(event: CdkDragDrop<itask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+  addTask() {
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done: false
+    })
+    this.todoForm.reset();
+    localStorage.setItem("ToDoTask",JSON.stringify(this.tasks))
+  }
+  updateTask() {
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.editEnabled = false;
+  }
+  deleteTask(i: number) {
+    this.tasks.splice(i, 1);
+  }
+  deleteDone(i: number) {
+    this.done.splice(i, 1)
+  }
+  onEditTask(item: itask, i: number) {
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updateIndex = i;
+    this.editEnabled = true;
+  }
+}
